5ff12f7c27cb343bbde62daf8341847f2f25bc58
arya <aryasadeghy@gmail.com>
2018-09-12 12:20:45 +0430

    make app screens

---
+++ screens/Trips.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+export default class Trips extends Component {
+    render(){
+        return(
+            <View> 
+                <Text> Trips Page </Text>
+            </View>
+        )
+    }
+}
