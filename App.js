0005bc9f643f809a772c0d02b87c45e37ad72655
arya <aryasadeghy@gmail.com>
2018-09-12 16:48:15 +0430

    add app.js

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Image, Dimensions } from 'react-native';
+
+//icon 
+import { Ionicons } from '@expo/vector-icons'
 //react navigation
 import { createBottomTabNavigator } from 'react-navigation'
 
@@ -8,40 +11,102 @@ import Explore from './screens/Explore'
 import Saved from './screens/Saved'
 import Trips from './screens/Trips'
 import Inbox from './screens/Inbox'
-
-
-// class App extends React.Component {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text>Open up App.js to start working on your app!</Text>
-//         <Text>Changes you make will automatically reload.</Text>
-//         <Text>Shake your phone to open the developer menu.</Text>
-//       </View>
-//     );
-//   }
-// }
-
-export default createBottomTabNavigator({
-  Explore: {
-    screen: Explore
-  },
-  Saved: {
-    screen: Saved
-  },
-  Trips: {
-    screen: Trips
-  },
-  Inbox: {
-    screen: Inbox
-  },
-})
-
+import Profile from './screens/Profile'
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  icon: {
+
+  },
+
+  tabBar: {
+    height: 52,
+    borderWidth: 0.5,
+    backgroundColor: 'white',
+  },
+
+  labelTabBar: {
+    fontSize: 12,
+    marginTop: 0, // <- This is the guilty one in my case, simply removed it and it worked as expected on iOS 10 & 11
+  },
+
+  indicatorTabBar: {
+    backgroundColor: 'black',
   },
 });
+
+
+
+
+export default createBottomTabNavigator({
+
+  Explore: {
+    screen: Explore,
+    navigationOptions: {
+      tabBarLabel: 'EXPLORE',
+      tabBarIcon: ({ tintColor }) => {
+        <Ionicons name="ios-search-outline" color={tintColor} size={24} />
+      }
+    }
+  },
+  Saved: {
+    screen: Saved,
+    navigationOptions: {
+      tabBarLabel: 'SAVED',
+      tabBarIcon: ({ tintColor }) => {
+        <Ionicons name="ios-heart-outline" color={tintColor} size={24} />
+      }
+    }
+  },
+  Trips: {
+    screen: Trips,
+    navigationOptions: {
+      tabBarLabel: 'TRIPS',
+      tabBarIcon: ({ tintColor }) => {
+        <Image source={require("./assets/airbnb.png")}
+          style={{ height: 24, width: 24, tintColor: tintColor }} />
+      }
+    }
+  },
+  Inbox: {
+    screen: Inbox,
+    navigationOptions: {
+      tabBarLabel: 'INBOX',
+      tabBarIcon: ({ tintColor }) => {
+        <Ionicons name="ios-chatboxes-outline" color={tintColor} size={24} />
+      }
+    }
+  },
+  Profile: {
+    screen: Profile,
+    navigationOptions: {
+      tabBarLabel: 'PROFILE',
+      tabBarIcon: ({ tintColor }) => {
+        <Ionicons name="ios-person-outline" color={tintColor} size={24} />
+      }
+    }
+  }
+}, {
+    tabBarOptions: {
+      activeTintColor: 'red',
+      inactiveTintColor: 'gray',
+      pressColor: 'blue',
+      showIcon: true,
+      iconStyle: {
+        width: 25,
+        height: 25,
+      },
+      style: styles.tabBar,
+      upperCaseLabel: false,
+      labelStyle: styles.labelTabBar,
+      indicatorStyle: styles.indicatorTabBar,
+      // style: {
+      //   backgroundColor: 'white',
+      //   borderTopWidth: 0,
+      //   shadowOffset: { width: 5, height: 3 },
+      //   shadowOpacity: 0.5,
+      //   elevation: 5,
+      // }
+    }
+
+
+  })
+
